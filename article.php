f1d2be610dd87bd43770118d0a1bb5c175cbdd8e
bbadrman <bbadrman@gmail.com>
2022-03-31 00:55:09 +0000

    REFACTORISATION DES REQUÊTES SQL

--- article.php
+++ article.php
@@ -37,27 +37,30 @@ if (!$article_id) {
  * 
  * PS : Vous remarquez que ce sont les mêmes lignes que pour l'index.php ?!
  */
-$pdo = getPdo();
+// $pdo = getPdo();
 /**
  * 3. Récupération de l'article en question
  * On va ici utiliser une requête préparée car elle inclue une variable qui provient de l'utilisateur : Ne faites
  * jamais confiance à ce connard d'utilisateur ! :D
  */
-$query = $pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
+// $query = $pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
 
-// On exécute la requête en précisant le paramètre :article_id 
-$query->execute(['article_id' => $article_id]);
+// // On exécute la requête en précisant le paramètre :article_id 
+// $query->execute(['article_id' => $article_id]);
 
-// On fouille le résultat pour en extraire les données réelles de l'article
-$article = $query->fetch();
+// // On fouille le résultat pour en extraire les données réelles de l'article
+// $article = $query->fetch();
+$article = findArticle($article_id);
 
 /**
  * 4. Récupération des commentaires de l'article en question
  * Pareil, toujours une requête préparée pour sécuriser la donnée filée par l'utilisateur (cet enfoiré en puissance !)
  */
-$query = $pdo->prepare("SELECT * FROM comments WHERE article_id = :article_id");
-$query->execute(['article_id' => $article_id]);
-$commentaires = $query->fetchAll();
+// $query = $pdo->prepare("SELECT * FROM comments WHERE article_id = :article_id");
+// $query->execute(['article_id' => $article_id]);
+// $commentaires = $query->fetchAll();
+
+$commentaires = findAllComments($article_id);
 
 /**
  * 5. On affiche 
--- save-comment.php
+++ save-comment.php
@@ -56,19 +56,21 @@ if (!$author || !$article_id || !$content) {
  * 
  * PS : Ca fait pas genre 3 fois qu'on écrit ces lignes pour se connecter ?! 
  */
-$pdo = getPdo();
+//$pdo = getPdo();
 
-$query = $pdo->prepare('SELECT * FROM articles WHERE id = :article_id');
-$query->execute(['article_id' => $article_id]);
+// $query = $pdo->prepare('SELECT * FROM articles WHERE id = :article_id');
+// $query->execute(['article_id' => $article_id]);
+$article = findArticle($article_id);
 
 // Si rien n'est revenu, on fait une erreur
-if ($query->rowCount() === 0) {
+if (!$article) {
     die("Ho ! L'article $article_id n'existe pas boloss !");
 }
 
 // 3. Insertion du commentaire
-$query = $pdo->prepare('INSERT INTO comments SET author = :author, content = :content, article_id = :article_id, created_at = NOW()');
-$query->execute(compact('author', 'content', 'article_id'));
+// $query = $pdo->prepare('INSERT INTO comments SET author = :author, content = :content, article_id = :article_id, created_at = NOW()');
+// $query->execute(compact('author', 'content', 'article_id'));
+insertComment($author , $content, $article_id);
 
 // 4. Redirection vers l'article en question :
 // header('Location: article.php?id=' . $article_id);
